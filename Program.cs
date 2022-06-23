a00703d2f6e886cdfd05012b9e784ff0318e23ee
MuraFederico <federicomura98@gmail.com>
2022-06-23 15:52:25 +0200

    implemented second bonus

--- Program.cs
+++ Program.cs
@@ -1,25 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string[] favFood = {"culurgiones", "seadas", "pizza", "shushi", "kebab"};
+string[] favFood = {"culurgiones", "seadas", "pizza", "sushi", "kebab"};
 
-Console.WriteLine("ho " + favFood.Length + " cibi preferiti\n");
 
-Console.WriteLine("e sono:\n");
+Console.WriteLine("inserisci il tuo cibo preferito:");
+
+string userFood = Console.ReadLine();
+Random rnd = new Random();
+string pcFood = favFood[rnd.Next(0, favFood.Length)];
+
+Console.WriteLine("i cibi preferiti sono " + favFood.Length + ":\n");
+
+while (!favFood.Contains(userFood))
+{
+    Console.WriteLine("il cibo non era tra i preferiti, riprova\n");
+    userFood = Console.ReadLine();
+    Console.WriteLine();
+}
 
 for (int i = 0; i < favFood.Length; i++)
 {
-    Console.WriteLine(favFood[i] + "\n");
+    if (favFood[i] == userFood && favFood[i] == pcFood){
+        Console.WriteLine(favFood[i] + " abbiamo gli stessi gusti\n");
+    }else if (favFood[i] == userFood)
+    {
+        Console.WriteLine(favFood[i] + " il tuo preferito\n");
+    }else if (favFood[i] == pcFood)
+    {
+        Console.WriteLine(favFood[i] + " il mio preferito\n");
+    }
+    else
+    {
+        Console.WriteLine(favFood[i] + "\n");
+    }
 }
 
 Console.WriteLine("quello che preferisco di piu' e' " + favFood[0] + "\n");
 
-Console.WriteLine("quello che preferisco di meno e' " + favFood[favFood.Length-1] + "\n");
+Console.WriteLine("quello che preferisco di meno e' " + favFood[favFood.Length - 1] + "\n");
 
 int halfValue = favFood.Length / 2;
 
-if(favFood.Length % 2 == 0)
+if (favFood.Length % 2 == 0)
 {
-    Console.WriteLine("i cibi a meta' classifica sono: " + favFood[halfValue-1] + " e " + favFood[halfValue]);
-}else
+    Console.WriteLine("i cibi a meta' classifica sono: " + favFood[halfValue - 1] + " e " + favFood[halfValue]);
+}
+else
 {
     Console.WriteLine("il cibo a meta' classifica e' " + favFood[halfValue]);
 }
