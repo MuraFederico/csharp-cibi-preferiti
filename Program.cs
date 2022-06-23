720e47e6e257018b77792956258d02501380ac28
MuraFederico <federicomura98@gmail.com>
2022-06-23 14:48:00 +0200

    exercise completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] favFood = {"culurgiones", "seadas", "pizza", "shushi", "kebab", "lasagne"};
+
+Console.WriteLine("ho " + favFood.Length + " cibi preferiti");
+Console.WriteLine("e sono:");
+for (int i = 0; i < favFood.Length; i++)
+{
+    Console.WriteLine(favFood[i]);
+}
+Console.WriteLine("quello che preferisco di piu' e' " + favFood[0]);
+Console.WriteLine("quello che preferisco di meno e' " + favFood[favFood.Length-1]);
