a73cec8c194ac168aba862f43194b96aa785f206
MuraFederico <federicomura98@gmail.com>
2022-06-23 15:07:38 +0200

    added empty lines between each output

--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string[] favFood = {"culurgiones", "seadas", "pizza", "shushi", "kebab"};
 
-Console.WriteLine("ho " + favFood.Length + " cibi preferiti");
-Console.WriteLine("e sono:");
+Console.WriteLine("ho " + favFood.Length + " cibi preferiti\n");
+
+Console.WriteLine("e sono:\n");
 
 for (int i = 0; i < favFood.Length; i++)
 {
-    Console.WriteLine(favFood[i]);
+    Console.WriteLine(favFood[i] + "\n");
 }
 
-Console.WriteLine("quello che preferisco di piu' e' " + favFood[0]);
-Console.WriteLine("quello che preferisco di meno e' " + favFood[favFood.Length-1]);
+Console.WriteLine("quello che preferisco di piu' e' " + favFood[0] + "\n");
+
+Console.WriteLine("quello che preferisco di meno e' " + favFood[favFood.Length-1] + "\n");
 
 int halfValue = favFood.Length / 2;
 
